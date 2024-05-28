1017342115317b4abbec51fd11683f4a8ccaefcd
RYU <takahashi.ryunosuke.job@gmail.com>
2024-05-28 10:56:30 +0900

    done section 5

--- App.js
+++ App.js
@@ -1,90 +1,56 @@
 import { StatusBar } from 'expo-status-bar';
-import { FlatList, SafeAreaView, ScrollView, View } from 'react-native';
+import { FlatList } from 'react-native';
 import ListItem from './components/list-item';
+import {
+  SafeAreaView,
+  SafeAreaProvider,
+} from 'react-native-safe-area-context';
+import { useEffect, useState } from 'react';
+import { NEWS_API_KEY } from '@env';
 
 export default function App() {
+  const [newsItems, setNewsItems] = useState([]);
 
-  const newsItems = [
-    {
-      id: 1,
-      title: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deleniti reprehenderit nam maxime iure cupiditate animi, excepturi fugiat vitae cumque vel.',
-      author: 'Tokyo News',
-      imageUrl: 'https://placehold.jp/100x100.png',
-    },
-    {
-      id: 2,
-      title: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deleniti reprehenderit nam maxime iure cupiditate animi, excepturi fugiat vitae cumque vel.',
-      author: 'Tokyo News',
-      imageUrl: 'https://placehold.jp/100x100.png',
-    },
-    {
-      id: 3,
-      title: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deleniti reprehenderit nam maxime iure cupiditate animi, excepturi fugiat vitae cumque vel.',
-      author: 'Tokyo News',
-      imageUrl: 'https://placehold.jp/100x100.png',
-    },
-    {
-      id: 4,
-      title: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deleniti reprehenderit nam maxime iure cupiditate animi, excepturi fugiat vitae cumque vel.',
-      author: 'Tokyo News',
-      imageUrl: 'https://placehold.jp/100x100.png',
-    },
-    {
-      id: 5,
-      title: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deleniti reprehenderit nam maxime iure cupiditate animi, excepturi fugiat vitae cumque vel.',
-      author: 'Tokyo News',
-      imageUrl: 'https://placehold.jp/100x100.png',
-    },
-    {
-      id: 6,
-      title: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deleniti reprehenderit nam maxime iure cupiditate animi, excepturi fugiat vitae cumque vel.',
-      author: 'Tokyo News',
-      imageUrl: 'https://placehold.jp/100x100.png',
-    },
-    {
-      id: 7,
-      title: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deleniti reprehenderit nam maxime iure cupiditate animi, excepturi fugiat vitae cumque vel.',
-      author: 'Tokyo News',
-      imageUrl: 'https://placehold.jp/100x100.png',
-    },
-    {
-      id: 8,
-      title: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deleniti reprehenderit nam maxime iure cupiditate animi, excepturi fugiat vitae cumque vel.',
-      author: 'Tokyo News',
-      imageUrl: 'https://placehold.jp/100x100.png',
-    },
-    {
-      id: 9,
-      title: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deleniti reprehenderit nam maxime iure cupiditate animi, excepturi fugiat vitae cumque vel.',
-      author: 'Tokyo News',
-      imageUrl: 'https://placehold.jp/100x100.png',
-    },
-    {
-      id: 10,
-      title: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deleniti reprehenderit nam maxime iure cupiditate animi, excepturi fugiat vitae cumque vel.',
-      author: 'Tokyo News',
-      imageUrl: 'https://placehold.jp/100x100.png',
-    },
-  ]
+  const URL = `https://newsapi.org/v2/top-headlines?country=jp&category=business&apiKey=${NEWS_API_KEY}`;
+
+  const fetchNews = async () => {
+    try {
+      const res = await fetch(URL)
+      const newsItems = await res.json()
+  
+      setNewsItems(newsItems.articles)
+    } catch (error) {
+      console.error('Error fetching news', error)
+    }
+  }
+
+
+  useEffect(() => {
+    fetchNews()
+  }, [])
 
   return (
-    <SafeAreaView
-      className="flex-1 bg-blue-300"
-
-    >
-      <FlatList
-        data={newsItems}
-        keyExtractor={item => item.id.toString()}
-        renderItem={({ item }) => (
-          <ListItem
-            title={item.title}
-            author={item.author}
-            image={item.imageUrl}
-          />
-        )}
-        className="bg-gray-100"
-      />
-      <StatusBar style="auto" />
-    </SafeAreaView>
+    <SafeAreaProvider>
+      <SafeAreaView
+        className="flex-1"
+        forceInset={{ top: 'never' }}
+      >
+        <FlatList
+          data={newsItems}
+          keyExtractor={item => item.id}
+          renderItem={({ item }) => (
+            <ListItem
+              title={item.title}
+              author={item.author}
+              image={item.urlToImage}
+            />
+          )}
+          className="bg-gray-100"
+        />
+        <StatusBar
+          animated={true}
+        />
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
